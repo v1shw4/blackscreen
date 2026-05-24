cead5aedb8328f8b2b9828731df44e7fa11c8bc8
KayT3 <sithija.ap@gmail.com>
2026-05-24 16:13:35 +0530

    Add Docker configuration and setup for Next.js application

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  outputFileTracingIncludes: {
+    '/*': ['node_modules/better-sqlite3/build/Release/*.node'],
+  },
 };
 
 export default nextConfig;
